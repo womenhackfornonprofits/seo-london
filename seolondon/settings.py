61b31c45aba60ddfe84a36eeff72b2e2ab38778f
Matt Lewsley <matt@glendelmdesign.co.uk>
2016-09-02 12:18:18 +0100

    Postgres setup

--- seolondon/settings.py
+++ seolondon/settings.py
@@ -190,17 +190,28 @@ CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
 
-DATABASES = {
-    'default': {
-        'CONN_MAX_AGE': 0,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': 'localhost',
-        'NAME': 'project.db',
-        'PASSWORD': '',
-        'PORT': '',
-        'USER': ''
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'seolondon',
+            'USER': 'seolondon',
+            'PASSWORD': 'seolondon',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 MIGRATION_MODULES = {
     
