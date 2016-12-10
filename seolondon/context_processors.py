ed71898f96e75608c8b671b7542724b0038622a3
Thomasina Lee <thomasina.lee@gmail.com>
2016-12-10 23:42:43 +0000

    rename tracking code to tracking id for better naming

--- seolondon/context_processors.py
+++ seolondon/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def google_analytics(request):
     return {
-       'GOOGLE_ANALYTICS_TRACKING_CODE':
+       'GOOGLE_ANALYTICS_TRACKING_ID':
            getattr(settings,
-		   'GOOGLE_ANALYTICS_TRACKING_CODE', '')
+		   'GOOGLE_ANALYTICS_TRACKING_ID', '')
     }
