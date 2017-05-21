c638bb64806f595d8c8c3edb5c5a5ec34c8a6ebe
Thomasina Lee <teelee7133@gmail.com>
2017-05-21 22:38:03 +0100

    do some admin tidy up

--- seo_post/admin.py
+++ seo_post/admin.py
@@ -29,6 +29,28 @@ class PostAdminForm(forms.ModelForm):
     class Meta:
         model = models.Post
         fields = '__all__'
+        help_texts = {
+            'author_image': (
+                '"author image" is only used for Blogs entries'
+            ),
+            'is_public': (
+                'Set this if it is ready to be shown to the public'
+            ),
+            'listing_image': (
+                'Set this for images to be shown in '
+                'news articles in the listing.  If not set, '
+                '"main_image" will be used'
+            ),
+            'main_image': (
+                'Image to be shown in the main article.  '
+                'Also used as fallback for news article listing image,'
+                'if listing_image is not set '
+            ),
+            'excerpt': (
+                'summary of the article to be displayed in the listing. '
+                'If not set, "body" will be shown instead'
+            ),
+        }
 
 
 @admin.register(models.Post)
@@ -36,16 +58,29 @@ class PostAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
 
     form = PostAdminForm
 
-    list_display = ('title', 'date_publish', 'author')
+    list_display = (
+        'title', 'post_type', 'is_public', 'date_publish',
+    )
 
     fieldsets = (
         (None, {
-            'fields': ('post_type', 'title', 'body', 'is_public')
+            'fields': (
+                'post_type', 'title', 'body', 'is_public', 'author'
+            )
+        }),
+        ('Images', {
+            'fields': (
+                'main_image', 'listing_image', 'author_image',
+            )
+        }),
+        ('Additional Text', {
+            'fields': (
+                'subtitle', 'excerpt',
+            ),
         }),
         ('Basic Options', {
+            'classes': ('collapse',),
             'fields': (
-                'main_image', 'listing_image', 'author', 'author_image',
-                'subtitle', 'excerpt',
                 'date_publish', 'date_expire', 'categories',
             ),
         }),
