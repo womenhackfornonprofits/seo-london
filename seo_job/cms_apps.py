d483ca42bb37f3fd4b022a42237954ac423850fd
T Lee <teelee7133@gmail.com>
2017-07-17 23:24:10 +0100

    fix error with old menu reference

--- seo_job/cms_apps.py
+++ seo_job/cms_apps.py
@@ -6,14 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 
-from seo_job.cms_menus import SeoJobMenu
-
 
 class SeoJobApphook(CMSApp):
     app_name = "seo_job"
     name = _("SEO Jobboard Application")
     urls = ["seo_job.urls"]
-    menus = [SeoJobMenu]
 
 
 apphook_pool.register(SeoJobApphook)  # register the application
